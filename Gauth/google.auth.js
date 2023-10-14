eed0902fbd1783840d05e01bebda678deb1cf1e5
LaxyaRupeja <altlakshi@gmail.com>
2023-10-14 21:42:52 +0530

    done

--- Gauth/google.auth.js
+++ Gauth/google.auth.js
@@ -5,10 +5,10 @@ const passport = require("passport");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: "https://transparent-potent-typhoon.glitch.me/auth/google/callback"
 },
     function (accessToken, refreshToken, profile, cb) {
-        
+
         cb(null, profile)
     }
 ));
